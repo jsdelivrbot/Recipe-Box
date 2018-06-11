507a84737c25ef6ae3d3ca936a31c2bdc7c82628
Oliver Yates <35773976+olijyat@users.noreply.github.com>
2018-06-11 12:15:03 +0200

    Add icons and improve grid layout

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { Header, Icon } from 'semantic-ui-react';
+import { Header, Icon, Image } from 'semantic-ui-react';
 
 const titleStyle = {
   color: '#4b4b4b',
   padding: '15px 30px'
 }
 
+const imageStyle = {
+  padding: '5px',
+  border: '2px solid #4b4b4b',
+  borderCollapse: 'separate',
+  
+  borderRadius: '50%'
+}
+
 const Title = props => (
   <div style={titleStyle}>
     <Header as='h1' icon textAlign='center'>
-      <Icon name='food' circular />
+      <Image style={imageStyle} src='https://uploads.codesandbox.io/uploads/user/e276cc26-2428-467a-a9cf-7ba3ffd6415c/8BGG-roller.png' circular />
       <Header.Content>Recipe-Box</Header.Content>
     </Header>
    
--- src/layout/GridLayout.js
+++ src/layout/GridLayout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Image, Divider } from 'semantic-ui-react';
 import Title from '../components/Title';
 import RecipeList from '../components/RecipeList';
 
@@ -7,11 +7,12 @@ const GridLayout = props => (
   <div>
     <Grid celled>
       <Grid.Row>
-        <Grid.Column width={3}>
-     
+        <Grid.Column width={2}>
+          <Image src='https://uploads.codesandbox.io/uploads/user/e276cc26-2428-467a-a9cf-7ba3ffd6415c/xeT5-soup.png' size='large'/>
         </Grid.Column>
-        <Grid.Column width={13}>
+        <Grid.Column width={12}>
          <Title />
+         <Divider />
         </Grid.Column>
       </Grid.Row>
 
@@ -21,10 +22,22 @@ const GridLayout = props => (
         </Grid.Column>
         <Grid.Column width={10}>
           <RecipeList />
+        
         </Grid.Column>
         <Grid.Column width={3}>
         
         </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row>
+        <Grid.Column width={3}>
+
+        </Grid.Column>
+        <Grid.Column width={13}>
+          <Divider />
+
+        </Grid.Column>
+        
       </Grid.Row>
     </Grid>
   </div>
