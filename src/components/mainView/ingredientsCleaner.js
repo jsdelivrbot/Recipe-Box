7a9c83b3df7c4ba492b506f05230826acd8b8d11
Oliver Yates <35773976+olijyat@users.noreply.github.com>
2018-06-19 17:12:18 +0200

    Change ingredients to key/value pairs.
     Update ingredientsCleaner accordingly

--- src/components/mainView/ingredientsCleaner.js
+++ src/components/mainView/ingredientsCleaner.js
@@ -5,9 +5,14 @@ const ingredientsCleaner = props => {
 
   let createList
   if (!Array.isArray(props)) {
-     const split = props.split(",");
-      createList = split.map(el => {
-      return <List.Item key={el.id}>{el}</List.Item>;
+    const ingredientsArray = Object.entries(props)
+    
+
+    createList = ingredientsArray.map(el => {
+      let [item, num] = el;
+    
+      const upper = item[0].toUpperCase() + item.substr(1);
+      return <List.Item key={el.id}>{upper} : {num}</List.Item>;
     });
   } else {
     createList = props.map(el => {
--- src/containers/RecipeBox.js
+++ src/containers/RecipeBox.js
@@ -25,15 +25,14 @@ import ErrorHandler from '../withErrorHandler/ErrorHandler'
       title: "Chicken Curry",
       image_url:
         "https://hips.hearstapps.com/del.h-cdn.co/assets/17/31/2048x1365/gallery-1501791674-delish-chicken-curry-horizontal.jpg?resize=980:*",
-      ingredients: `2 tbsp. extra-virgin olive oil,
-                  1 medium yellow onion, chopped,
-                  2 lb. boneless skinless chicken breasts,
-                  3 cloves garlic, minced,
-                  1 tbsp. freshly grated ginger,
-                  1 tsp. paprika,
-                  1 tsp. turmeric,
-                  1 tsp. coriander,
-                  1/2 tsp. cumin`,
+      ingredients: {
+        oliveOil: 2,
+        onion: 1,
+        garlic: 3,
+        ginger: 1,
+        coriander: 1,
+        cumin: 0.5
+      }, 
       directions: `In a large pot over medium-high heat, heat oil. Add onion and cook until softened and lightly golden, 5 to 7 minutes. Add chicken and sear until golden on all sides, 5 minutes more. Stir in garlic and ginger and cook until fragrant, 2 minutes more.
                   Coat aromatics in spices and cook until very fragrant, less than a minute more. Pour in tomatoes and chicken broth and bring to a simmer.
                   Stir in heavy cream, then season with salt and pepper. Simmer until chicken pieces are cooked through and tender, about 10 minutes.
