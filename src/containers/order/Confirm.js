36b31e69ed88c17002fc66da955b470d04ea00c0
Oliver Yates <35773976+olijyat@users.noreply.github.com>
2018-06-25 10:50:44 +0200

    Move confirmation modals to their own stateful container
    Link up with router - makes navigation easier

--- src/containers/order/Confirm.js
+++ src/containers/order/Confirm.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Grid, Image, Divider, Header, Search, Card } from 'semantic-ui-react';
 import Title from '../../components/Title';
 import SearchRecipes from '../../components/search/Search';
+import ConfirmationModal from './ConfirmationModal';
 
-export default class WhoWeAre extends React.Component {
+export default class Confirm extends React.Component {
  
-
-
   render() {
+    const recipeInfo = props.mainRecipe;
 
+    const Info = <DeliveryForm
+      updateDelivery={props.updateDelivery} />
+   
     return (
       <div>
         <Grid celled stackable>
@@ -17,16 +20,27 @@ export default class WhoWeAre extends React.Component {
               <Image className='icon' src='https://uploads.codesandbox.io/uploads/user/e276cc26-2428-467a-a9cf-7ba3ffd6415c/xeT5-soup.png' size='large' />
             </Grid.Column>
             <Grid.Column width={12}>
-              
+              <Title />
+              <Divider />
             </Grid.Column>
           </Grid.Row>
 
           <Grid.Row>
-            <Grid.Column width={3}>
-             
-            </Grid.Column>
+
             <Grid.Column width={12}>
-             
+              <Header >You are close to getting your delicious meal! </Header>
+              Add your delivery information below
+              {Info}
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={12}>
+              <ConfirmationModal
+                deliverInfo={props.deliverInfo}
+                recipeInfo={recipeInfo}
+                orderRecipe={props.orderRecipe}
+                orderAccepted={props.orderAccepted}
+                orderLoaded={props.orderLoaded} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
---
+++ src/containers/order/ConfirmationModal.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Modal, Message, Grid, Image, Divider, Header, Search, Card, List } from 'semantic-ui-react';
+import Title from '../../components/Title';
+import SearchRecipes from '../../components/search/Search';
+import StepsFirst from './steps/StepsFirst';
+import StepsSecond from './steps/StepsSecond';
+import Loader from '../Loader';
+import DeliveryForm from './DeliveryForm';
+
+export default class ConfirmationModal extends React.Component {
+  state = {
+    open: false,
+    orderSent: false,
+  };
+
+  open = () => this.setState({ open: true });
+  close = () => this.setState({ open: false });
+
+  orderSent() {
+    this.setState({ orderSent: true })
+    this.props.orderRecipe();
+  }
+
+  render() {
+    console.log(this.props)
+    const { open } = this.state;
+    const ingredientsArray = Object.entries(this.props.recipeInfo.ingredients)
+    const ingredientsDisplay = ingredientsArray.map(el => {
+      let [item, num] = el;
+
+      const upper = item[0].toUpperCase() + item.substr(1);
+      return <List.Item key={el.id}>{upper} : {num}</List.Item>;
+    });
+    const recipeTitle = this.props.recipeInfo.title;
+    const Loading = <div className='loader'>Laoding.. </div>
+    const successMsg = (
+      <Message positive>
+        <Message.Header> Success!</Message.Header>
+        <p>Your food is on its way!! Enjoy!!</p>
+      </Message>
+    );
+    const failureMsg = (
+      <Message negative>
+        <Message.Header>Something went wrong!</Message.Header>
+        <p>Please try again</p>
+      </Message>
+    );
+    console.log(this.props)
+    return (
+      <Modal
+        scrolling
+        dimmer={false}
+        open={open}
+        onOpen={this.open}
+        onClose={this.close}
+        closeIcon
+        size="fullscreen"
+        trigger={
+          <Button primary icon>
+            Proceed <Icon name="right chevron" />
+
+          </Button>
+        }
+      >
+        <Modal.Header>Finalise Order</Modal.Header>
+        <Modal.Content scrolling>
+          <Button primary Icon onClick={this.orderSent.bind(this)}> Order!</Button>
+          <p>Is this information correct?</p>
+          {(this.state.orderSent && !this.props.orderAccepted) ? Loading : null}
+          {(this.props.orderAccepted && this.props.orderLoaded) ? successMsg : null}
+          {(!this.props.orderAccepted && this.props.orderLoaded) ? failureMsg : null}
+          <Header as='h3'>{recipeTitle}</Header>
+          <Header as='h3'>{recipeTitle}</Header>
+          <List bulleted>
+            {ingredientsDisplay}
+          </List>
+
+          <br />
+          <Header as='h3'>Delivery Details</Header>
+          {this.props.deliverInfo !== undefined ? this.props.deliveryInfo : null}
+          <Button primary Icon onClick={this.orderSent.bind(this)}> Order!</Button>
+
+        </Modal.Content>
+        <Modal.Actions>
+
+
+          <StepsSecond />
+        </Modal.Actions>
+      </Modal>
+    );
+  }
+}
+
+
+  
