82ce89878e215787cf83aed17ec2fc68ba90be4c
Oliver Yates <35773976+olijyat@users.noreply.github.com>
2018-07-05 12:21:54 +0200

    Install redux devtools as well as thunk using combine

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from 'redux-thunk';
 
@@ -24,9 +24,10 @@ const rootReducer = combineReducers({
   replaceMain
 })
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
   );
 
 class Index extends React.Component {
