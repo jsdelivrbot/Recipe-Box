ef38cd52445b7e0c5ff69e7b0f5b739672c4d529
Oliver Yates <35773976+olijyat@users.noreply.github.com>
2018-07-06 11:48:38 +0200

    Add purchaseOrder actionTypes and actionCreators

---
+++ src/store/actions/order/postPurchaseOrder.js
@@ -0,0 +1,53 @@
+import * as actionTypes from "./actionTypes";
+import axios from "../../../components/axios-orders";
+
+export const purchaseOrderSuccess = (orderStatus, id) => {
+  return {
+    type: actionTypes.PURCHASE_ORDER_SUCCESS,
+    orderStatus,
+    id
+  };
+};
+
+export const purchaseOrderFail = orderStatus => {
+  return {
+    type: actionTypes.PURCHASE_ORDER_FAIL,
+    orderStatus
+  };
+};
+
+export const purchaseOrderStart = (instructions, deliveryInfo) => {
+  const price = "$20";
+  const order = {
+    instructions,
+    price,
+    deliveryInfo
+  };
+
+  return dispatch => {
+    axios
+      .post("orders.json", order)
+      .then(response => {
+        if (response !== undefined) {
+          const orderStatus = {
+            orderLoaded: true,
+            orderAccepted: true
+          };
+          dispatch(purchaseOrderSuccess(orderStatus));
+        } else {
+          const orderStatus = {
+            orderLoaded: true,
+            orderAccepted: false
+          };
+          dispatch(purchaseOrderFail(orderStatus));
+        }
+      })
+      .catch(error => {
+        const orderStatus = {
+          orderLoaded: true,
+          orderAccepted: false
+        };
+        dispatch(purchaseOrderFail(orderStatus));
+      });
+  };
+};
